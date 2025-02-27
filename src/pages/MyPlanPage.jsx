d85e2504415e081ce8f762fe9822f114eefb2e75
SuimKim <github suim0215@gmail.com>
2025-02-27 14:37:47 +0900

    design:#6 기본 레이아웃 구성

--- src/pages/MyPlanPage.jsx
+++ src/pages/MyPlanPage.jsx
@@ -1,5 +1,16 @@
+import MyPlanCard from '../components/features/MyPlanCard';
+
 const MyPlanPage = () => {
-  return <div>MyPlanPage</div>;
+  return (
+    <div className="p-10">
+      <h1 className="font-bold text-3xl text-center">수임이가 계획한 여행 일정 리스트</h1>
+      <div className="grid gap-10 m-12 grid-cols-3 justify-evenly">
+        <MyPlanCard />
+        <MyPlanCard />
+        <MyPlanCard />
+      </div>
+    </div>
+  );
 };
 
 export default MyPlanPage;
