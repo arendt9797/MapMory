963d8046a694fafa40a85b39cf53d6efcbea1d48
SuimKim <github suim0215@gmail.com>
2025-03-01 06:18:26 +0900

    refactor:#6 컴포넌트로 수정

--- src/components/features/myPlanPage/StatusPage.jsx
+++ src/components/features/myPlanPage/StatusPage.jsx
@@ -1,11 +1,14 @@
 import { AUTH_STORAGE } from '../../../constants/storageKey';
+import Title from '../components/commons/Title';
 
 const StatusPage = ({ children }) => {
   const nickname = JSON.parse(localStorage.getItem(AUTH_STORAGE)).state.userInfo.nickname;
 
   return (
     <main className="p-10 flex flex-col justify-center items-center">
-      <h1 className="font-bold text-3xl text-center m-5 text-secondary">{nickname}의 여행 계획 리스트</h1>
+      <Title fontSize={'3xl'} margin={'md'}>
+        {nickname}의 여행 계획 리스트
+      </Title>
       <div className="w-4/5 flex justify-center h-52 items-center">{children}</div>
     </main>
   );
