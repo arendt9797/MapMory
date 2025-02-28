e749e085a98d5b147b4161cf654ec8a4c55f525f
hyerin-kang <hkang0420@gmail.com>
2025-02-28 15:19:48 +0900

    헤더 실제 로그인상태 반영

--- src/components/layouts/AuthHeader.jsx
+++ src/components/layouts/AuthHeader.jsx
@@ -1,7 +1,15 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { CREATEPLAN, MYPLAN } from '../../constants/pagePaths';
+import { useAuthStore } from '../../stores/authStore';
 
 export const AuthHeader = () => {
+  const userSignOut = useAuthStore((state) => state.userSignOut);
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    userSignOut();
+    alert('로그아웃 되었습니다.');
+    navigate('/');
+  };
   return (
     <div className="flex items-center gap-4">
       <Link to={CREATEPLAN} className="text-secondary hover:text-primaryHover">
@@ -10,7 +18,9 @@ export const AuthHeader = () => {
       <Link to={MYPLAN} className="text-secondary  hover:text-primaryHover">
         내 여행 일정
       </Link>
-      <button className="bg-secondary p-2 px-4 rounded-3xl text-white  hover:bg-secondaryHover">로그아웃</button>
+      <button onClick={handleLogout} className="bg-secondary p-2 px-4 rounded-3xl text-white  hover:bg-secondaryHover">
+        로그아웃
+      </button>
     </div>
   );
 };
