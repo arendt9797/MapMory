4af43b97a948c85892943464526c6a11dab72ba4
SuimKim <github suim0215@gmail.com>
2025-02-28 17:15:51 +0900

    refactor:#6 코드 리팩토링 - Api 불러오는 함수 분리 - 조건부 렌더링 코드 수정

--- src/pages/MyPlanPage.jsx
+++ src/pages/MyPlanPage.jsx
@@ -1,37 +1,32 @@
 import { useQuery } from '@tanstack/react-query';
-import { supabase } from '../lib/apis/supabaseClient';
 import MyPlanCard from '../components/features/MyPlanCard';
+import { getMyPlans } from '../lib/apis/planApi';
 
 const MyPlanPage = () => {
-  const getMyPosts = async () => {
-    const { data } = await supabase.from('plans').select(`* , detail_plans(place, plan_date, plan_time, map)`);
-    return data;
-  };
+  const testId = 'd0d35038-4aac-4eb1-a625-0775e2c64087';
 
   const {
-    data: testData,
-    isPending: isTestPending,
-    isError: isTestError
+    data: planData,
+    isPending: isDataPending,
+    isError: isDataError
   } = useQuery({
-    queryFn: getMyPosts,
+    queryFn: () => getMyPlans(testId),
     queryKey: ['plans']
   });
 
-  if (isTestError) {
-    return <div>에러</div>;
-  }
-  if (isTestPending) {
-    return <div>로딩</div>;
-  }
-
   return (
     <div className="p-10 flex flex-col justify-center items-center">
-      <h1 className="font-bold text-3xl text-center m-5">닉네임의 여행 계획 리스트</h1>
-      <div className="w-4/5 grid grid-cols-3 gap-20 p-10">
-        {testData.map((item) => (
-          <MyPlanCard key={item.id} item={item} />
-        ))}
-      </div>
+      <h1 className="font-bold text-3xl text-center m-5 text-secondary">닉네임의 여행 계획 리스트</h1>
+      {isDataError && <div className="w-4/5 flex justify-center h-52 items-center">오류가 발생했습니다.</div>}
+      {isDataPending && <div className="w-4/5 flex justify-center h-52 items-center">로딩중입니다.</div>}
+      {!isDataError && !isDataPending && planData && (
+        <div className="w-4/5 grid grid-cols-3 gap-20 p-10">
+          {planData.map((item) => (
+            <MyPlanCard key={item.id} item={item} />
+          ))}
+        </div>
+      )}
+      {/* <button className="bg-primary p-2 px-4 rounded-3xl text-white hover:bg-primaryHover">더보기 +</button> */}
     </div>
   );
 };
