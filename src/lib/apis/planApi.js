e977655b426879ba3e96a8cba7e309d20aa791f3
SuimKim <github suim0215@gmail.com>
2025-03-01 02:30:58 +0900

    refactor:#6 select 컬럼 수정

--- src/lib/apis/planApi.js
+++ src/lib/apis/planApi.js
@@ -3,7 +3,7 @@ import { supabase } from './supabaseClient';
 export const getMyPlans = async (id) => {
   const { data } = await supabase
     .from('plans')
-    .select(`* , detail_plans(place, plan_date, plan_time, map)`)
+    .select(`* , detail_plans(place, plan_date, plan_time)`)
     .eq('user_id', id);
   return data;
 };
