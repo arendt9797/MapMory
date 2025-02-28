3739743040556d12844187baa4ecbfba36fb7a3a
SuimKim <github suim0215@gmail.com>
2025-03-01 02:22:25 +0900

    refactor:#6 데이터 가공 로직 수정

--- src/components/features/MyPlanCard.jsx
+++ src/components/features/MyPlanCard.jsx
@@ -3,21 +3,22 @@ import { Link } from 'react-router-dom';
 const MyPlanCard = ({ item }) => {
   const { id, title, detail_plans } = item;
 
-  // 배열.reduce((누적값, 현잿값, 인덱스, 요소) => { return 결과 }, 초깃값);
-  const testArr = detail_plans.reduce((acc, current) => {
-    const newArr = { place: current.place, plan_time: current.plan_time };
-    return { ...acc, [current.plan_date]: [...(acc[current.plan_date] || []), newArr] };
+  const sortList = [...detail_plans].sort((a, b) => a.plan_date.localeCompare(b.plan_date));
+
+  const datedPlansObj = sortList.reduce((acc, cur) => {
+    acc[cur.plan_date] = acc[cur.plan_date] || [];
+    acc[cur.plan_date].push(cur.place);
+    return acc;
   }, {});
 
-  // 위에서 만든 객체를 key로 돌려서 새로운 객체 return
-  const resultArray = Object.entries(testArr)
-    .map(([date, plans]) => ({ date, plans }))
-    .sort((a, b) => a.date.localeCompare(b.date));
+  const datedPlansArr = Object.entries(datedPlansObj).map(([date, place]) => {
+    return { date: date, places: place };
+  });
 
-  const placeArray = resultArray.map((item) => ({
-    date: item.date,
-    places: item.plans.map((plan) => plan.place) // plans 배열에서 place만 추출
-  }));
+  const startDate = datedPlansArr[0].date;
+  const endDate = datedPlansArr[datedPlansArr.length - 1].date;
+
+  const test = datedPlansArr.map((item, index) => `${index + 1}일차: ${item.places.join(', ')}`);
 
   return (
     <Link to={`/detailPlan/${id}`}>
@@ -26,16 +27,10 @@ const MyPlanCard = ({ item }) => {
         <div className="h-48 bg-gray-100 flex justify-center items-center">지도 이미지가 들어갈</div>
         {/*contents box*/}
         <div className="p-4">
-          <p className="text-sm text-secondary">
-            {resultArray[0].date} ~ {resultArray[resultArray.length - 1].date}의 일정
-          </p>
+          <p className="text-sm text-secondary">{`${startDate} ~ ${endDate}의 일정`}</p>
           <h2 className="text-xl font-bold text-secondary mt-2">{title}</h2>
-          <div className="text-gray-500 mt-2 overflow-hidden line-clamp-3 h-14 text-sm">
-            {placeArray.map((item, index) => (
-              <p key={index}>
-                {index + 1}일차: {item.places.join(', ')}
-              </p>
-            ))}
+          <div className="text-gray-500 mt-2 overflow-hidden line-clamp-3 h-14 text-sm whitespace-pre-line">
+            <p>{test.map((plan) => plan + '\n')}</p>
           </div>
         </div>
       </div>
