a17c5acbcc8492fc92f1f47aab6586052c426bb8
SuimKim <github suim0215@gmail.com>
2025-02-28 14:03:52 +0900

    feat:#6 My Plan Card 컴포넌트 구현 - supabase 데이터 가공해서 출력하기 - 카드 클릭 시 상세 페이지로 이동

--- src/components/features/MyPlanCard.jsx
+++ src/components/features/MyPlanCard.jsx
@@ -1,17 +1,45 @@
-const MyPlanCard = () => {
+import { Link } from 'react-router-dom';
+
+const MyPlanCard = ({ item }) => {
+  const { id, title, detail_plans } = item;
+
+  // 배열.reduce((누적값, 현잿값, 인덱스, 요소) => { return 결과 }, 초깃값);
+  const testArr = detail_plans.reduce((acc, current) => {
+    const newArr = { place: current.place, plan_time: current.plan_time };
+    return { ...acc, [current.plan_date]: [...(acc[current.plan_date] || []), newArr] };
+  }, {});
+
+  // 위에서 만든 객체를 key로 돌려서 새로운 객체 return
+  const resultArray = Object.entries(testArr)
+    .map(([date, plans]) => ({ date, plans }))
+    .sort((a, b) => a.date.localeCompare(b.date));
+
+  const placeArray = resultArray.map((item) => ({
+    date: item.date,
+    places: item.plans.map((plan) => plan.place) // plans 배열에서 place만 추출
+  }));
+
   return (
-    <div className="flex flex-col justify-center items-start gap-8 w-80 h-96 border-2 border-black">
-      <div className="w-full h-64 bg-slate-400">이미지가 들어갈</div>
-      <div className="flex flex-col gap-3">
-        <p className="text-secondary">2025.02.25 - 2025.02.27의 일정</p>
-        <h2 className="font-bold text-2xl text-secondary">제주도를 가봅시다!</h2>
-        <p className="text-gray-500">
-          1일차: 공항-{'>'}어디어디-{'>'}어디갔다가-{'>'}숙소 <br />
-          2일차: 숙소-{'>'}어디어디-{'>'}어디갔다가-{'>'}숙소 <br />
-          3일차: 숙소-{'>'}어디어디-{'>'}어디갔다가-{'>'}공항
-        </p>
+    <Link to={`/detailPlan/${id}`}>
+      <div className=" rounded-3xl overflow-hidden bg-white border-2 w-72 hover:shadow-lg transition-shadow duration-300">
+        {/* 지도 이미지 플레이스홀더 */}
+        <div className="h-48 bg-gray-100 flex justify-center items-center">지도 이미지가 들어갈</div>
+        {/*contents box*/}
+        <div className="p-4">
+          <p className="text-sm text-secondary">
+            {resultArray[0].date} ~ {resultArray[resultArray.length - 1].date}의 일정
+          </p>
+          <h2 className="text-xl font-bold text-secondary mt-2">{title}</h2>
+          <div className="text-gray-500 mt-2 overflow-hidden line-clamp-3 h-14 text-sm">
+            {placeArray.map((item, index) => (
+              <p key={index}>
+                {index + 1}일차: {item.places.join(', ')}
+              </p>
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
