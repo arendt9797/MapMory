6accfeb9fb58a63b96ef90981c7ac82fb5b24ab3
SuimKim <github suim0215@gmail.com>
2025-03-03 15:19:37 +0900

    refactor:#22 코드 수정

--- src/components/commons/Text.jsx
+++ src/components/commons/Text.jsx
@@ -1,18 +1,22 @@
 /**
- * text 컴포넌트입니다. 기본 p 태그로 설정
+ * Text 컴포넌트입니다. 기본 p 태그로 설정
  * @component
  * @example
- * <Text fontSize={'md'} textColor={'secondary'} align={'center'}>
- *  내용이 들어가면 됩니다.
- * </Text>
+ * <p theme='secondary' size='md' className='m-5'>
+ *   내용입니다.
+ * </p>
  *
- * @prop {string} [fontSize] - 글자 크기 ( "xs" | "sm" | "md" | "lg" | "xl" | "2xl" )
- * @prop {string} [textColor] - 색상 클래스명 ex)black, gray-500
- * @prop {string} [align] - 정렬 위치 ( "left" | "center" | "right" )
+ * @prop {string} [theme] - 기본 테마, 프로젝트 primary 색상 or secondary 색상 선택
+ * @prop {string} [size] - 텍스트 크기 ( "xs" | "sm" | "md" | "lg" | "xl" | "2xl" )
+ * @prop {string} [className] - 그 외 추가적인 스타일 유틸리티 클래스
  */
+const Text = ({ children, theme, size, className = '', ...props }) => {
+  const themeStyles = {
+    primary: 'text-primary',
+    secondary: 'text-bg-secondary'
+  };
 
-const Text = ({ fontSize, textColor, align, children }) => {
-  const sizeValue = {
+  const sizeStyles = {
     xs: 'text-xs',
     sm: 'text-sm',
     md: 'text-md',
@@ -21,13 +25,13 @@ const Text = ({ fontSize, textColor, align, children }) => {
     '2xl': 'text-2xl'
   };
 
-  const alignValue = {
-    left: 'text-left',
-    center: 'text-center',
-    right: 'text-right'
-  };
+  const styles = `${themeStyles[theme]} ${sizeStyles[size]} ${className}`;
 
-  return <p className={`${sizeValue[fontSize]} text-${textColor} ${alignValue[align]}`}>{children}</p>;
+  return (
+    <p className={styles} {...props}>
+      {children}
+    </p>
+  );
 };
 
 export default Text;
