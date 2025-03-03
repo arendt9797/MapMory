695ef3bff283bddd05d7482fdc4843da33cbeb01
SuimKim <github suim0215@gmail.com>
2025-03-03 21:56:45 +0900

    refactor:#34 날짜순 정렬 코드 삭제 - api 호출 때 정렬하도록 order 구문 추가

--- src/lib/utils/processPlansData.js
+++ src/lib/utils/processPlansData.js
@@ -1,7 +1,5 @@
 const processPlansData = (detail_plans) => {
-  const sortList = [...detail_plans].sort((a, b) => a.plan_date.localeCompare(b.plan_date));
-
-  const datedPlansObj = sortList.reduce((acc, cur) => {
+  const datedPlansObj = detail_plans.reduce((acc, cur) => {
     acc[cur.plan_date] = acc[cur.plan_date] || [];
     acc[cur.plan_date].push(cur.place);
     return acc;
