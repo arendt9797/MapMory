0df09ffbc777b61314d95270e32626471f63d5fe
SuimKim <github suim0215@gmail.com>
2025-03-03 22:15:43 +0900

    refactor:#34 API 호출 로직수정 - 날짜순 정렬 order() 추가 - useInfiniteQuery 사용을 위한 상수 및 로직 추가

--- src/lib/apis/planApi.js
+++ src/lib/apis/planApi.js
@@ -1,9 +1,14 @@
 import { supabase } from './supabaseClient';
 
-export const getMyPlans = async (id) => {
+export const getMyPlans = async (id, pageParam) => {
+  const limit = 3;
+  const offset = (pageParam - 1) * limit;
+
   const { data } = await supabase
     .from('plans')
     .select(`* , detail_plans(place, plan_date, plan_time)`)
-    .eq('user_id', id);
+    .eq('user_id', id)
+    .order('plan_date', { referencedTable: 'detail_plans', ascending: true })
+    .range(offset, offset + limit - 1);
   return data;
 };
