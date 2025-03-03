8c4677ff8ac7894e39f610fbe6eac07eebc17d86
SuimKim <github suim0215@gmail.com>
2025-03-03 22:16:50 +0900

    feat:#34 더보기 기능 추가 - 3개씩 출력

--- src/pages/MyPlanPage.jsx
+++ src/pages/MyPlanPage.jsx
@@ -10,25 +10,25 @@ const MyPlanPage = () => {
   const testId = 'd0d35038-4aac-4eb1-a625-0775e2c64087';
   const nickname = JSON.parse(localStorage.getItem(AUTH_STORAGE)).state.userInfo.nickname;
 
-  const { planData, isDataError, isDataPending } = useMyPlan(testId);
+  const { data, error, fetchNextPage, hasNextPage, isFetching, isLoading } = useMyPlan(testId);
 
-  if (isDataError) {
+  if (error) {
     return <StatusPage>오류가 발생했습니다.</StatusPage>;
   }
-  if (isDataPending) {
+  if (isLoading) {
     return <StatusPage>로딩중입니다.</StatusPage>;
   }
   return (
     <main className="p-10 flex flex-col justify-center items-center m-">
-      <Title size={'3xl'}>{nickname}의 여행 계획 리스트</Title>
-      <GridBox>
-        {planData.map((item) => (
-          <MyPlanCard key={item.id} item={item} />
-        ))}
-      </GridBox>
-      <Button theme="primary" size="md">
-        더보기 +
-      </Button>
+      <Title size={'3xl'} className="m-5">
+        {nickname}의 여행 계획 리스트
+      </Title>
+      <GridBox>{data?.pages.map((page) => page.map((item) => <MyPlanCard key={item.id} item={item} />))}</GridBox>
+      {hasNextPage && (
+        <Button theme="primary" size="md" onClick={() => fetchNextPage()}>
+          {isFetching ? '로딩중...' : '더보기 +'}
+        </Button>
+      )}
     </main>
   );
 };
