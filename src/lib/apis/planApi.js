f941e477066a04872d998875c8517054f30512d4
SuimKim <github suim0215@gmail.com>
2025-03-04 10:21:01 +0900

    refactor:#34 API 호출 함수 상수화

--- src/lib/apis/planApi.js
+++ src/lib/apis/planApi.js
@@ -4,10 +4,10 @@ import { supabase } from './supabaseClient';
 export const getMyPlans = async (id, limit, pageParam) => {
   const offset = (pageParam - 1) * limit;
   const { data } = await supabase
-    .from('plans')
-    .select(`* , detail_plans(place, plan_date, plan_time)`)
-    .eq('user_id', id)
-    .order('plan_date', { referencedTable: 'detail_plans', ascending: true })
+    .from(supabaseItems.PLANS)
+    .select(`* , ${supabaseItems.DETAILPLANS}(*)`)
+    .eq(supabaseItems.USER_ID, id)
+    .order(supabaseItems.PLANDATE, { referencedTable: supabaseItems.DETAILPLANS, ascending: true })
     .range(offset, offset + limit - 1);
   return data;
 };
