90db6e99b9b822c3fbe7540b1659c8e5c61145e5
SuimKim <github suim0215@gmail.com>
2025-03-04 11:14:30 +0900

    refactor:#34 카드에 뜨는 데이터 메모로 변경

--- src/lib/utils/processPlansData.js
+++ src/lib/utils/processPlansData.js
@@ -1,12 +1,12 @@
 const processPlansData = (detail_plans) => {
   const datedPlansObj = detail_plans.reduce((acc, cur) => {
     acc[cur.plan_date] = acc[cur.plan_date] || [];
-    acc[cur.plan_date].push(cur.place);
+    acc[cur.plan_date].push(cur.plan_memo);
     return acc;
   }, {});
 
-  const datedPlansArr = Object.entries(datedPlansObj).map(([date, place]) => {
-    return { date: date, places: place };
+  const datedPlansArr = Object.entries(datedPlansObj).map(([date, memo]) => {
+    return { date: date, memo: memo };
   });
 
   return datedPlansArr;
