f25463b28c8ce372fc7418fd3264df674f7f9db9
SuimKim <github suim0215@gmail.com>
2025-03-04 15:36:23 +0900

    Merge branch 'dev' of https://github.com/arendt9797/MapMory into Feature/#34-MyPlanPage_더보기

--- src/lib/apis/planApi.js
+++ src/lib/apis/planApi.js
@@ -1,23 +1,14 @@
 import { supabaseItems } from '../../constants/supabaseConstants';
 import { supabase } from './supabaseClient';
+import { getUserId } from './supabaseGetUserId';
 
 export const getMyPlans = async (limit, pageParam) => {
-  const getUserId = async () => {
-    const { data, error } = await supabase.auth.getUser();
-    if (error) {
-      alert('유저 정보를 가져오는 중 오류 발생:', error.message);
-      return null;
-    }
-    return data.user.id;
-  };
   const userId = await getUserId();
-
   const offset = (pageParam - 1) * limit;
   const { data, error } = await supabase
     .from(supabaseItems.PLANS)
     .select(`* , ${supabaseItems.DETAILPLANS}(*)`)
     .eq(supabaseItems.USER_ID, userId)
-    // .order('detail_plans(plan_date)', { ascending: true })
     .order(supabaseItems.PLANDATE, { referencedTable: supabaseItems.DETAILPLANS, ascending: true })
     .range(offset, offset + limit - 1);
   if (error) alert('에러 발생, ', error.message);
